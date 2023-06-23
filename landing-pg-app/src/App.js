73a3da0a9a5cc7a4adaecf11ef7d4b272297a7b6
psarode-theorem <psarode@theoremlabs.io>
2023-06-23 14:07:09 -0400

    Created Modal for Signin

--- landing-pg-app/src/App.js
+++ landing-pg-app/src/App.js
@@ -7,7 +7,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 function FeatureCards() {
   return (
@@ -17,19 +21,14 @@ function FeatureCards() {
 
     <style type="text/css">
                 {`
-            .myCardTitle{
-              background-color: purple;
+            .famioloyStyledCardTitle{
+              background-color: black;
               color: white;
+              padding: 0.5rem;
+              font-size: 1.5rem;
+              font-weight:normal;
             }
-            .super-btn-flat {
-              background-color: purple;
-              color: white;
-            }
-
-            .super-btn-xxl {
-              padding: 1rem 1.5rem;
-              font-size: 4.5rem;
-            }
+            
             `}
       </style>
 
@@ -38,8 +37,8 @@ function FeatureCards() {
         <Col>
               <Card bg="light" style={{ height: '20rem' }} >
                         <Card.Body>
-                          <Card.Title>Affordable</Card.Title>
-                          <Card.Subtitle className="mb-2 text-muted">Using scale of modern technology</Card.Subtitle>
+                          <Card.Title bsPrefix="famioloyStyledCardTitle" >Affordable</Card.Title>
+                          <Card.Subtitle className="mb-2">Using scale of modern technology</Card.Subtitle>
                           <Card.Text>
                           <p>You don't have to be a $50M and greater house-hold to own the benefits of Family Office. Famiology has used power of AI & Cloud scale technologies to build Family-Office-as Service offering that provide some of same services enjoyed by only Ultra rich.</p>
                           </Card.Text>
@@ -52,8 +51,8 @@ function FeatureCards() {
         <Col>
               <Card bg="light" style={{ height: '20rem' }}>
                         <Card.Body>
-                          <Card.Title>Multiple Integrations</Card.Title>
-                          <Card.Subtitle className="mb-2 text-muted">We are integrated with many providers</Card.Subtitle>
+                          <Card.Title bsPrefix="famioloyStyledCardTitle" >Multiple Integrations</Card.Title>
+                          <Card.Subtitle className="mb-2">We are integrated with many providers</Card.Subtitle>
                           <Card.Text>
                           <p>Famiology recognizes you are t`ired of on-boarding and sharing same private data across multiple providers when managing your life events.Famiology has kept the Family Household at the center and flipped the discovery and service from Providers up-side-down. 
                           So you don't register into various Provider portals and lock-boxes even when you are still shopping for best Providers. Instead, invite Providers to your familiar FamilyOffice expereicne from Famiology.</p>
@@ -67,10 +66,14 @@ function FeatureCards() {
         <Col>
               <Card bg="light" style={{ height: '20rem' }} >
                         <Card.Body>
-                          <Card.Title>Own your data</Card.Title>
-                          <Card.Subtitle className="mb-2 text-muted">Data Privacy Smart</Card.Subtitle>
+                          <Card.Title bsPrefix="famioloyStyledCardTitle">Own your data</Card.Title>
+                          <Card.Subtitle className="mb-2">Data Privacy Smart</Card.Subtitle>
                           <Card.Text>
-                          <p>You are not on-boarding into Providers portals and locks-boxes, instead Providers onboard and work with you in your FamilyOffice experience.This means you are custodian of your own data and selectively shared what you need share with Providers and only what's needed for the context of your service need.</p>
+                          <p>
+                            You are not on-boarding into Providers portals and locks-boxes, instead Providers onboard and work with 
+                            you in your FamilyOffice experience.This means you are custodian of your own data and selectively shared 
+                            what you need share with Providers and only what's needed for the context of your service need.
+                          </p>
                           </Card.Text>
                           
                           <Button variant="dark">Learn More</Button>
@@ -87,12 +90,17 @@ function FeatureCards() {
 }
 
 function FamiologyHeader(){
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   function handleSignin(){
     alert('You clicked me!');
   }
 
   return(
+    <>
   <div class="container-fluid">
     <div class="row bg-dark text-white">
   
@@ -107,12 +115,51 @@ function FamiologyHeader(){
       <div class="col-sm-2">
           <div class="btn-group btn-group-md">
               <Button  variant="light">Subscribe</Button>
-              <Button onClick={handleSignin} variant="light">Sign-in</Button>
+              <Button onClick={handleShow} variant="light">Sign-in</Button>
               
           </div>
       </div>
     </div>
   </div>
+
+    <Modal  show={show} 
+            onHide={handleClose} 
+            size="md"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Sign-in into Famiology</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          
+        <FloatingLabel controlId="floatingInputGrid" label="Email address">
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+        <br/>
+        
+        <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+       
+        </Modal.Body>
+        <Modal.Footer>
+        <br/>
+        <Button variant="dark" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="dark" onClick={handleClose}>
+            Sign-in
+          </Button>
+          
+          <p class="sm"> 
+              <a href="#"> Forgot User Id   </a> | <a href="#"> Forgot Password </a> 
+              </p>
+          
+        </Modal.Footer>
+      </Modal>
+
+  </>
   );
 }
 
